70f45340cdaf4bd2c7e4e407535bcc492629f3a9
izan garcia <izangarcia408@gmail.com>
2026-04-21 11:46:43 +0200

    feat: maquetado de inicio con carrusel y seccion quienes somos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './pages/inicio/inicio.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { ProductoDetalleComponent } from './pages/producto-detalle/producto-detalle.component';
 import { CarritoPagoComponent } from './pages/carrito-pago/carrito-pago.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
 
 export const routes: Routes = [
   {
