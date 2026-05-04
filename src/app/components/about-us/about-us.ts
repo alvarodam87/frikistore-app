d62e7db39274c10912dc68dad40bf3fa5b2e592c
Luis Perez <argenisperezcabrera@gmail.com>
2026-05-04 12:57:13 +0200

    agregar componente aboutUS

--- src/app/components/about-us/about-us.ts
+++ src/app/components/about-us/about-us.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
-export class AboutUs {}
+export class AboutUs {
+  // CLAVE: Debes definir esta lista para que el HTML pueda leerla[cite: 25]
+  valores = [
+    { icono: 'bi-gem', titulo: 'Autenticidad', desc: 'Solo productos 100% originales.' },
+    { icono: 'bi-heart-fill', titulo: 'Pasión', desc: 'Somos coleccionistas ayudando a coleccionistas.' },
+    { icono: 'bi-shield-check', titulo: 'Confianza', desc: 'Envíos protegidos y garantizados.' }
+  ];
+}
