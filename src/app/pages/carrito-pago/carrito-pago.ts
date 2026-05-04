6e810cf67a71e025924ee28aa9891754ca3dafd8
Luis Perez <argenisperezcabrera@gmail.com>
2026-05-04 12:47:00 +0200

    agregar formulario de pago

--- src/app/pages/carrito-pago/carrito-pago.ts
+++ src/app/pages/carrito-pago/carrito-pago.ts
@@ -1,17 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common'; // He quitado NgOptimizedImage de aquí
 import { RouterLink } from '@angular/router';
-// CORRECCIÓN: Ruta al archivo .ts del servicio real
+import { FormsModule, NgForm } from '@angular/forms';
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-carrito-pago',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule], // También lo he quitado de este array
   templateUrl: './carrito-pago.html',
   styleUrl: './carrito-pago.css'
 })
 export class CarritoPago {
-  // CLAVE: 'public' para que el HTML no dé error TS2339
   public cartService = inject(CartService);
+
+  // Variable para controlar metodo de pago
+  metodoSeleccionado: string = '';
+
+  procesarCompra(formulario: NgForm) {
+    if (formulario.valid && this.metodoSeleccionado) {
+      console.log('Datos de Envío:', formulario.value);
+      console.log('Método de Pago:', this.metodoSeleccionado);
+
+      alert('¡Pedido realizado con éxito, Nakama! Revisa tu email.');
+    }
+  }
 }
--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -6,27 +6,26 @@ import { FormsModule, NgForm } from '@angular/forms';
   selector: 'app-contacto',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './contacto.html'
+  templateUrl: './contacto.html',
+  styleUrl: './contacto.css'
 })
 export class Contacto {
   mensajeEnviado = false;
   errorEnvio = false;
 
-  enviarMensaje(form: NgForm) {
-    if (form.invalid) {
+  enviarMensaje(formulario: NgForm) {
+    if (formulario.valid) {
+      console.log('Mensaje recibido:', formulario.value);
+      this.mensajeEnviado = true;
+      this.errorEnvio = false;
+
+      // Limpiar el formulario después de 3 segundos
+      setTimeout(() => {
+        formulario.reset();
+        this.mensajeEnviado = false;
+      }, 4000);
+    } else {
       this.errorEnvio = true;
-      this.mensajeEnviado = false;
-      return;
     }
-
-    // Si todo está bien
-    this.errorEnvio = false;
-    this.mensajeEnviado = true;
-    form.resetForm();
-
-    // Ocultar el mensaje de éxito después de 5 segundos
-    setTimeout(() => {
-      this.mensajeEnviado = false;
-    }, 5000);
   }
 }
