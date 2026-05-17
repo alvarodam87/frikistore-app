5a7711e77b5a7f6ab113120508fcb09211a5b2cb
Alvaro <alvarodam87@gmail.com>
2026-05-17 17:34:15 +0200

    feat: add accesorios component

---
+++ src/app/pages/accesorios/accesorios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accesorios',
+  imports: [],
+  templateUrl: './accesorios.html',
+  styleUrl: './accesorios.css',
+})
+export class Accesorios {}
